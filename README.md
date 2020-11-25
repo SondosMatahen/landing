0e91513d440dd3511c61879f23d9382992d07ad8
SondosMatahen <sondosmatahen@gmail.com>
2020-11-25 16:31:02 +0200

    edit read me

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [Prerequisite](#Prerequisite)
+* [Installation](#Installation)
+* [Runtime Environment](#Runtime)
 
 ## Instructions
 
@@ -11,3 +14,19 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+## Prerequisite
+
+To be able to read this code you should have idea about HTML , CSS ,JS(DOM)
+I used event listner to make the project more dynmaic.
+
+## Installation
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+
+## Runtime Environment
+
+* JS6
+* DOM
--- js/app.js
+++ js/app.js
@@ -23,87 +23,78 @@ const items = document.querySelectorAll('section');
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
 */
 
+//Bulid nav elements for menu 
 items.forEach(element => {
-    // console.log('item', element)
-    //     const item=document.createElement('li');
-    //    item.textContent=`${element.dataset.nav}`
     const item = `<li class='menu__link ${element.className}' data-set=${element.id}><a href="#${element.id}">${element.dataset.nav}</li>`;
     list.insertAdjacentHTML('beforeend', item)
 });
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-// Scroll to anchor ID using scrollTO event
+// Scroll to section on link click by listening to event listner(click) in nav list
+// edit the section class to 'your-active-class' if it's selected from the nav list
 list.addEventListener('click', e => {
+
+    items.forEach(se => {
+        console.log('before', se)
+        if (se.classList.contains('your-active-class')) {
+            se.classList.remove('your-active-class');
+            console.log('after', se)
+        }
+    });
+
     e.preventDefault();
     const par = e.target.hasAttribute('data-set') ? e.target : e.target.parentElement;
-    // par.classList.toggle('active')
-    const toScroll=document.getElementById(par.dataset.set);
-    console.log('parent datat',toScroll);
-    toScroll.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    const toScroll = document.getElementById(par.dataset.set);
+    toScroll.classList.add('your-active-class');
+    // setTimeout(()=>{
+    //     toScroll.classList.remove('active');
+    // },1000)
+    console.log('parent datat', toScroll);
+    toScroll.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 })
 
 
-
-items.forEach(section=>{
-    section.addEventListener('mouseover', e => {
+//add eventlistener (sroll) to each section which make edit on the active section
+// Also iterate through the nav list to edit the active one depend in the active (scrolled) section.
+items.forEach(section => {
+    section.addEventListener('mousemove', e => {
         // console.log('e',e)
         e.preventDefault();
 
-        // const ul=document.querySelectorAll('.menu__link');
-        // ul.forEach(li=>{
-        //     if(li.classList.contains('active'))
-        //     {list.classList.remove('active')
-        // console.log('li',ul)}
-        // })
-        const par = e.target.hasAttribute('section') ?e.target:e.target.parentElement;
-        // par.classList.toggle('active')
-        const li=document.querySelector(`.menu__link[data-set='${section.id}']`)
-        console.log('parent datat',par);
-        // console.log('liiiii',li)
+        const ul = document.querySelectorAll('.menu__link');
+        ul.forEach(li => {
+            console.log('li', li)
+            if (li.classList.contains('active')) {
+                li.classList.remove('active')
+                console.log('li after', li)
+            }
+        });
+
+        items.forEach(se => {
+            console.log('before', se)
+            if (se.classList.contains('active')) {
+                se.classList.remove('active');
+                console.log('after', se)
+            }
+        });
+
+        const par = e.target.hasAttribute('section') ? e.target : e.target.parentElement;
+        const li = document.querySelector(`.menu__link[data-set='${section.id}']`)
+        console.log('parent', par);
         section.classList.add('active');
         li.classList.add('active')
 
-        setTimeout(()=>{
-            section.classList.remove('active');
-            li.classList.remove('active')
-        },1000)
-       
-    
+        // setTimeout(() => {
+        //     section.classList.remove('active');
+        //     li.classList.remove('active')
+        // }, 1000)
+
+
     })
-})
+});
 
 
 
 
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-// let options = {
-//     root:null,
-//     rootMargin: '0px',
-//     threshold: 1.0
-// }
-
-// const obs=new IntersectionObserver(callback,option)
